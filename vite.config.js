11054b044ad7bf9eabd1f993af81930a90b143a5
¨oscar <josuemareno010203@gmail.com>
2025-10-26 19:09:56 -0400

    Prepara app para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/tareas-app/", // <- reemplaza "tareas-app" por el nombre exacto de tu repo
   plugins: [react()],
-})
+});
+  
